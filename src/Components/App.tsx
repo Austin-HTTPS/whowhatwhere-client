2b566fe353fd093664280aaa1074166108cc6bd9
shmeegie <ericellb@gmail.com>
2020-04-15 22:22:07 -0400

    feat(ui): rework of landing page

--- src/Components/App.tsx
+++ src/Components/App.tsx
@@ -6,7 +6,9 @@ import io from 'socket.io-client';
 import PersonIcon from '@material-ui/icons/Person';
 import MeetingRoomIcon from '@material-ui/icons/MeetingRoom';
 import LockIcon from '@material-ui/icons/Lock';
-import HelpOutlineIcon from '@material-ui/icons/HelpOutline';
+import UserIcon from '../icons/user.svg';
+import LobbyIcon from '../icons/lobby.svg';
+import PasswordIcon from '../icons/password.svg';
 
 const darkTheme = createMuiTheme({
   palette: {
@@ -38,6 +40,10 @@ function App() {
   const [questionAnswer, setQuestionAnswer] = useState('');
   const [gameInProgress, setGameInProgress] = useState(Boolean);
   const [gameAnswers, setGameAnswers] = useState<{ question: string, answer: string}[][]>([]);
+
+  const [lobbyError, setLobbyError] = useState({error: false, message: ''});
+  const [usernameError, setUserNameError] = useState({error: false, message: ''});
+  const [passwordError, setPasswordError] = useState({error: false, message: ''});
 
   useEffect(() => {
     const setupSocket = async () => {
@@ -103,13 +109,22 @@ function App() {
 
   const createGame = () => {
     if (lobbyName === '' || username === '') {
-      setError(true);
-      setErrorText('Lobbyname / Username is required');
+      if (!lobbyName) {
+        setLobbyError({error:true, message: "Lobby Name cannot be empty"})
+      } else {
+        setLobbyError({error:false, message: ''})
+      }
+      if (!username) {
+        setUserNameError({error:true, message: "Username cannot be empty"})
+      } else {
+        setUserNameError({error:true, message: ''})
+      }
       return false;
     }
 
-    setError(false);
-    setErrorText('');
+    setLobbyError({error: false, message: ''});
+    setUserNameError({error: false, message: ''});
+    setPasswordError({error: false, message: ''});
     const createGameCredentials = { lobbyName, username, password };
     console.log('trying')
     socket.emit('createGame', createGameCredentials)
@@ -150,7 +165,7 @@ function App() {
     <ThemeProvider theme={darkTheme}>
       <div className="App">
         <div style={{width: "100%"}}>
-        {/* { (gameOwner !== username && !gameInProgress && playerInLobby) && 
+        { (gameOwner !== username && !gameInProgress && playerInLobby) && 
           <div> Waiting for {gameOwner} to start the game </div>
         }
 
@@ -190,21 +205,20 @@ function App() {
             </div>
           </div>
           </div>
-        } */}
+        }
 
         { !playerInLobby &&
           <div className="createOrJoinLobbyContainer">
             <div className="introductionSection"> 
               <div className="introTitleContainer">
-                <div className="introIcon"><HelpOutlineIcon></HelpOutlineIcon></div>
+                <div className="introIcon">?!</div>
                 <div className="introTitle">Who What Where</div>
               </div>
               <div className="introContent"> 
-                Pellentesque mollis arcu ac quam interdum scelerisque. <br/><br/>
-                Phasellus sollicitudin purus facilisis mollis tincidunt. 
-                Aenean laoreet cursus laoreet. Interdum et malesuada fames ac ante ipsum primis in faucibus. 
-                Sed risus lacus, ornare sit amet rutrum non, egestas a ipsum. 
-                Suspendisse potenti 
+                An Interactive Story building game to play with friends. <br/><br/>
+                Challenge your friends to a game of Who, What, Where? An Ad-libs inspired interactive story building game 
+                where you and your friends answer a series of 9 questions. The answers get mashed together to create wild stories. <br/><br/>
+                <div className="extraPlayerText">Best played with 3-9 players </div>
               </div>
               <div className="divider"></div>
             </div> 
@@ -212,29 +226,35 @@ function App() {
             <div className="createJoin"> 
               <div className="textFieldContainer">
                 <div className="textFieldTags">
-                  <PersonIcon className="textFieldIcon"></PersonIcon>
+                  <img className="textFieldIcon" src={UserIcon} />
                   <div className="textFieldTitle">Username *</div>
                 </div>
-                <TextField className="textField" required color="primary" variant="outlined" id="standard-basic" onChange={(e) => setLobbyName(e.target.value) } value={lobbyName}/>
+                <TextField autoComplete="off" className="textField" required color="primary" variant="outlined" id="standard-basic" onChange={(e) => setUserName(e.target.value) } value={username}/>
+                <div className="textFieldError"></div>
+                { lobbyError.error && <div className="textFieldError2">{lobbyError.message}</div>}
               </div>
               <div className="textFieldContainer">
                 <div className="textFieldTags">
-                  <MeetingRoomIcon className="textFieldIcon"></MeetingRoomIcon>
+                <img className="textFieldIcon" src={LobbyIcon} />
                   <div className="textFieldTitle">Lobby Name *</div>
                 </div>
-                <TextField className="textField" required color="primary" variant="outlined" id="standard-basic" onChange={(e) => setUserName(e.target.value) } value={username}/>
+                <TextField autoComplete="off" className="textField" required color="primary" variant="outlined" id="standard-basic" onChange={(e) => setLobbyName(e.target.value) } value={lobbyName}/>
+                <div className="textFieldError"></div>
+                { usernameError.error && <div className="textFieldError2">{usernameError.message}</div>}
               </div>
               <div className="textFieldContainer">
                 <div className="textFieldTags">
-                  <LockIcon className="textFieldIcon"></LockIcon>
+                  <img className="textFieldIcon" src={PasswordIcon} />
                   <div className="textFieldTitle">Password</div>
                 </div>
-                <TextField className="textField" color="primary" variant="outlined" id="standard-basic" onChange={(e) => setPassword(e.target.value) } value={password}/>
+                <TextField autoComplete="off" type="password" className="textField" color="primary" variant="outlined" id="standard-basic" onChange={(e) => setPassword(e.target.value) } value={password}/>
+                <div className="textFieldError"></div>
+                { passwordError.error && <div className="textFieldError2">{passwordError.message}</div>}
               </div>             
               <div className="createJoinButtonsContainer">
                 {error && <div style={{color: 'red', fontSize: "14px", margin: '12px'}}>{errorText}</div>}
-                <Button className="createButton" color="primary" onClick={() => joinGame()}> Join Game </Button>
-                <Button className="joinButton" color="primary" onClick={() => createGame()}> Create Game </Button>
+                <Button className="createButton" color="primary" onClick={() => createGame()}> Create Game </Button>
+                <Button className="joinButton" color="primary" onClick={() => joinGame()}> Join Game </Button>
               </div>
             </div>
             </div>
