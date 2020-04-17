ee6c5d40770b2294186aeb1effa37f8875fab09c
ericellb <ericellb@gmail.com>
2020-04-17 01:07:00 -0400

    Updates

---
+++ precache-manifest.1d71d2676dbf5afac2a5c4067748af26.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb811acf7da5e8e435e1d0d326c23fa7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a19bf6cb2556ebaa462f",
+    "url": "/static/css/main.50cc12f6.chunk.css"
+  },
+  {
+    "revision": "4cfacb963f2703a4c0e0",
+    "url": "/static/js/2.9be2ec1e.chunk.js"
+  },
+  {
+    "revision": "3be25bf51018a491ef7f73dfd2dd74c4",
+    "url": "/static/js/2.9be2ec1e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a19bf6cb2556ebaa462f",
+    "url": "/static/js/main.a5cc6152.chunk.js"
+  },
+  {
+    "revision": "6f0234b4b87d585d06e8",
+    "url": "/static/js/runtime-main.cfae3202.js"
+  },
+  {
+    "revision": "9e894d6447ebd8b4d5d5afbc888f1a02",
+    "url": "/static/media/RedHatDisplay-Black.9e894d64.ttf"
+  },
+  {
+    "revision": "b1c152bb20fffab8db5551832d2c48f2",
+    "url": "/static/media/RedHatDisplay-Bold.b1c152bb.ttf"
+  },
+  {
+    "revision": "ece83a2583223497d95d1195546b170b",
+    "url": "/static/media/RedHatDisplay-Medium.ece83a25.ttf"
+  },
+  {
+    "revision": "e4690c0771f951cf1a0efa6ab8d01ec1",
+    "url": "/static/media/RedHatDisplay-Regular.e4690c07.ttf"
+  },
+  {
+    "revision": "11eabca2251325cfc5589c9c6fb57b46",
+    "url": "/static/media/Roboto-Regular.11eabca2.ttf"
+  },
+  {
+    "revision": "d10239bd4b606498e9673ffd1050d088",
+    "url": "/static/media/lobby.d10239bd.svg"
+  },
+  {
+    "revision": "1b3f94dcc93d39b34c944adc8e3b7567",
+    "url": "/static/media/password.1b3f94dc.svg"
+  },
+  {
+    "revision": "a2bcb8f53810471db68b18c8dd148329",
+    "url": "/static/media/question.a2bcb8f5.svg"
+  },
+  {
+    "revision": "7de5e5f7d3fbd734c8d9328cc6806365",
+    "url": "/static/media/user.7de5e5f7.svg"
+  }
+]);
